6b613050b4257e846ad70f57dfa054dfa5e7f9d0
jestin skaria <jestinskaria15@gmail.com>
2019-11-28 13:57:40 -0500

    first commit

---
+++ app/src/main/java/com/example/mainproject/SixRowActivity.java
@@ -0,0 +1,14 @@
+package com.example.mainproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SixRowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_six_row);
+    }
+}
