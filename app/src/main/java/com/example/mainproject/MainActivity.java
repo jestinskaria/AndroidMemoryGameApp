c962c1a8fe0a9139354677a15f9af485a9d9ecb4
jestin skaria <jestinskaria15@gmail.com>
2019-11-29 15:03:46 -0500

    changes

--- app/src/main/java/com/example/mainproject/MainActivity.java
+++ app/src/main/java/com/example/mainproject/MainActivity.java
@@ -13,13 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button btn_4_start = (Button) findViewById(R.id.btn4Start);
         Button button = (Button) findViewById(R.id.button35);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btn_4_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               int start = 1;
+               int end = 50;
+
             }
         });
     }
